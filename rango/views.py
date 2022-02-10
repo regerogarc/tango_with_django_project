a90e1ecbd5284dd485c1977386a0485a2f59631e
Finlay Durkin <finlaydurkin@gmail.com>
2022-02-10 17:29:45 +0000

    COOKIeS Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
@@ -23,11 +24,39 @@ def index(request):
     context_dict["categories"] = category_list
     context_dict["pages"] = page_list
 
-    return render(request, "rango/index.html", context = context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, "rango/index.html", context = context_dict)
+
+    return response
+
+def get_server_side_cookie(request, cookie, defualt_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = defualt_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
 
 def about(request):
     
-    context_dict = {"boldmessage": "This tutorial has been put together by Mr Incredible"}
+    context_dict = {"boldmessage": "This tutorial has been put together by Finlay Durkin"}
+    
+    visitor_cookie_handler(request)
+    context_dict["visits"] = request.session['visits']
     
     return render(request, "rango/about.html", context = context_dict)
 
